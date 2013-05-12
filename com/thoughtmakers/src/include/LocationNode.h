9da89b962836b064ed771ad7386b33ed2c45ede1
madan1988 <madan.ase@gmail.com>
2013-05-12 10:55:23 +0530

    Added LocationNode to build tree

---
+++ com/thoughtmakers/src/include/LocationNode.h
@@ -0,0 +1,6 @@
+struct location_node
+{
+location loc_data;
+location_node *left;
+location_node *right;
+};
--- com/thoughtmakers/src/location/location_mem_structure.cpp
+++ com/thoughtmakers/src/location/location_mem_structure.cpp
@@ -3,46 +3,47 @@
 #include "../include/location_mtd_proto.h"
 #include<fstream>
 #include<string>
-
+#include<vector>
 using namespace std;
-string files[255];
+vector<string> locations;
 int main()
 {
-readLocationFiles();
-buildBST();
-return 0;
+	readLocationFiles();
+	buildBST();
+	return 0;
 }
 
 void readLocationFiles()
 {
-string line;
-int line_index = 0;
-ifstream locFile;
-locFile.open("../../test/LOC_INFO.txt");
-if(locFile.is_open())
-{
-while(!locFile.eof())
-{
-getline(locFile,line);
-cout<<line<<endl;
-files[line_index++] = line;
-}
-locFile.close();
-}
-else
-{
-cout<<"Unable to open file"<<endl;
-}
+	string line;
+	int line_index = 0;
+	ifstream locFile;
+	locFile.open("../../test/LOC_INFO.txt");
+	if(locFile.is_open())
+	{
+		while(!locFile.eof())
+		{
+			getline(locFile,line);
+			if(!line.empty())
+				locations.push_back(line);
+		}
+		locFile.close();
+	}
+	else
+	{
+		cout<<"Unable to open file"<<endl;
+	}
 }
 
 void buildBST()
 {
-int index=0;
-while(!files[index].empty())
-{
-insertNode(root,files[index]);
-cout<<files[index]<<endl;
-index++;
-}
+	int index=0;
+	/*while(!files[index].empty())
+	  {
+	  insertNode(root,files[index]);
+	  cout<<files[index]<<endl;
+	  index++;
+	  }
+	 */
 }
 
