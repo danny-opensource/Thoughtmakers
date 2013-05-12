a759ec5f22a6db4e8bc3d042a91e08ff9bd23d27
madan1988 <madan.ase@gmail.com>
2013-05-12 15:28:53 +0530

    Added comments for location prototype header

--- com/thoughtmakers/src/include/location_mtd_proto.h
+++ com/thoughtmakers/src/include/location_mtd_proto.h
@@ -1,7 +1,12 @@
+/*
+ * Define all the method prototypes used by location_mem_structure.cpp
+ */
+#ifndef LOCATION_METHOD_PROTOTYPES
+#define LOCATION_METHOD_PROTOTYPES
 #include "LocationNode.h"
-#include<string>
 void readLocationFiles();
 void buildBST();
 void insertIntoTree(std::string);
 void insert(location_node*,std::string);
 void debugPrintTree(location_node*);
+#endif
