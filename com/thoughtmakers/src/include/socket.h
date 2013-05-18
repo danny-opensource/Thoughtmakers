860e286bdabfe4bd076f8eea57c7a418333164d9
root <root@karthik-VirtualBox.(none)>
2013-05-19 00:52:41 +0530

    adding socket fucntionality

---
+++ com/thoughtmakers/src/include/socket.h
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netdb.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <string>
+#include <arpa/inet.h>
+
+
+const int MAXHOSTNAME = 200;
+const int MAXCONNECTIONS = 5;
+const int MAXRECV = 500;
+
+struct  Socket
+{
+ public:
+  Socket();
+  ~Socket();
+
+  // Server initialization
+  bool create();
+  bool bind ( const int port );
+  bool listen() const;
+  bool accept ( Socket& ) const;
+
+  // Client initialization
+  bool connect ( const std::string host, const int port );
+
+  // Data Transimission
+  bool send ( const std::string ) const;
+  int recv ( std::string& ) const;
+
+
+  void set_non_blocking ( const bool );
+
+  bool is_valid() const { return m_sock != -1; }
+
+ 
+
+  int m_sock;
+  sockaddr_in m_addr;
+
+
+};
+
+
